fd81b2adfc0b4f96bff95d1b3d05fcdc879f220b
PradeepMoolemane <pradeepmc.816@gmail.com>
2025-03-11 10:20:51 +0530

    eslint errors

--- blocks/socialicons/socialicons.js
+++
@@ -1,7 +0,0 @@
-export default async function decorate(block) {
-  // decorate footer DOM
-  block.textContent = '';
-  const footer = document.createElement('div');  
-  block.append(footer);
-}
-  
