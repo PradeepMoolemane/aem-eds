d33cec71199fbaf3ebd9930d81fc4e0c50f1240e
PradeepMoolemane <pradeepmc.816@gmail.com>
2025-03-11 10:28:02 +0530

    eslint errors latest

--- helix-pages/sitemap.js
+++ helix-pages/sitemap.js
@@ -19,7 +19,7 @@ const urls = [
 const generateSitemap = () => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${urls.map(url => `
+  ${urls.map((url) => `
     <url>
       <loc>${url.loc}</loc>
       <lastmod>${url.lastmod}</lastmod>
