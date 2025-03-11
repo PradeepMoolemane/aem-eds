0f9886ae921ae629f5cfe723426ecb8f5e8d0fc0
PradeepMoolemane <pradeepmc.816@gmail.com>
2025-03-11 10:26:32 +0530

    eslint errors

--- helix-pages/sitemap.js
+++ helix-pages/sitemap.js
@@ -6,17 +6,17 @@ const urls = [
     loc: 'https://main--aem-eds--pradeepmoolemane.aem.page/',
     lastmod: '2023-10-01',
     changefreq: 'daily',
-    priority: 1.0
+    priority: 1.0,
   },
-  { 
+  {
     loc: 'https://main--aem-eds--pradeepmoolemane.aem.page/about',
     lastmod: '2023-10-01',
     changefreq: 'monthly',
-    priority: 0.8
-  }
+    priority: 0.8,
+  },
 ];
 
-const generateSitemap = (urls) => {
+const generateSitemap = () => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${urls.map(url => `
@@ -31,7 +31,7 @@ const generateSitemap = (urls) => {
   return sitemap;
 };
 
-const sitemap = generateSitemap(urls);
+const sitemap = generateSitemap();
 const sitemapPath = path.join(__dirname, 'sitemap.xml');
 
 fs.writeFileSync(sitemapPath, sitemap, 'utf8');
